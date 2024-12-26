4f41e7b655e22b9013eec1ca94b731cdb1ddbf62
Eliangangs <eliangangs@gmail.com>
2024-12-26 16:29:48 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+let time = document.querySelector('.time');
+let bg = document.querySelector('.wrapper');
+let hero_bg = document.querySelector('.hero');
+let btn = document.querySelector('.btn');
+let del = document.querySelector('.samovnyshenie');
+let img = document.querySelector('.img')
+let time_bg = document.querySelector('.time-box');
+
+btn.addEventListener('click', funTic);
+let time_ = setInterval(ticTac, 1000);
+function ticTac() {
+  time.textContent = moment().format('HH:mm:ss');
+}
+function funTic() {
+  clearInterval(time_);
+  time_bg.style.background = '#fff';
+  time.style.color = '#000'
+  img.style.display = 'none';
+  hero_bg.style.backgroundImage = 'url(../img/bg.png)';
+  bg.style.backgroundColor = '#fff'
+  del.style.display = 'none';
+}
